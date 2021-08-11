1718c57b41ede83f561f6ec53d08e178b1e83185
Karthiseeni <karthiseeni96@gmail.com>
2021-08-11 11:53:54 +0530

    mobile responsive

--- src/components/featureHero/FeatureHero.js
+++ src/components/featureHero/FeatureHero.js
@@ -6,7 +6,7 @@ function FeaturesHero({
 }) {
   return (
     <>
-      <div className="col-xl-4 col-md-6 col-sm-12">
+      <div className="col-xl-4 col-md-6 col-sm-12 px-4 ">
         <img className='featuresImg' src={img} alt={alt} />
         <h5 className='subHeading'>{headline}</h5>
         <p className="para">{description} </p>
--- src/components/featureHero/FeatureSection.js
+++ src/components/featureHero/FeatureSection.js
@@ -9,7 +9,7 @@ function FeatureSection() {
     <div className='feature'>
       <div className="container">
         <h2 >Why First-Time OKR Users Prefer</h2>
-        <h2 className='featureHeader'>Synergita OKR?</h2>
+        <h2 className='featureHeader pb-4'>Synergita OKR?</h2>
         <div className="row">
           <FeaturesHero {...featureObjOne} />
           <FeaturesHero {...featureObjTwo} />
